e5dba8151b9b3552279d6639185edb7846534b66
TheSmartMonkey <vandellelaurent@yahoo.fr>
2025-05-22 09:45:37 +0200

    move scripts to root

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,15 +1,16 @@
 // @ts-ignore
 import { writeFile } from 'fs';
+import path from 'path';
 import { jsonFileNames, jsonFiles } from './helpers/model';
 
 function main() {
   for (const name of Object.values(jsonFileNames)) {
     const json = JSON.stringify(jsonFiles[name], null, 2);
-    writeFile(`${name}.json`, json, (err) => {
+    const filePath = path.join(__dirname, '..', 'assets', 'complex_modifications', `${name}.json`);
+    writeFile(filePath, json, (err) => {
       if (err) console.error(err);
       console.log(name);
     });
   }
 }
-
 main();
